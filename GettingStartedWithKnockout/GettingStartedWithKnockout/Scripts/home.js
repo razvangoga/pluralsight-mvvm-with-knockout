8272b6a2bd13564b053310ec4752675d0db8cc93
razvan goga <razvan.goga@gmail.com>
2012-10-02 13:12:06 +0300

    part 1 : getting started with ko

---
+++ GettingStartedWithKnockout/GettingStartedWithKnockout/Scripts/home.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="knockout-2.1.0.debug.js" />
+/// <reference path="jquery-1.8.2-vsdoc.js" />
+
+$(function () {
+    var viewModel = {
+        firstName: ko.observable("john");
+    };
+    ko.applyBindings(viewModel);
+});
