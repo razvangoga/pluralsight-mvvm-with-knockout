ed0fcb0c2c4c46f5e19908ce263f8dd312c366ba
razvan goga <razvan.goga@gmail.com>
2012-10-03 18:26:44 +0300

    After Module 6 Demo 3

--- KnockoutMVVM Module 6/KnockoutMVVM/js/binding-handlers.js
+++ KnockoutMVVM Module 6/KnockoutMVVM/js/binding-handlers.js
@@ -31,4 +31,35 @@
             $(element).button("option", "disabled", value.enabled === false);
         }
     };
+
+
+    ko.bindingHandlers.starRating = {
+        init: function (element, valueAccessor) {
+            $(element).addClass("starRating");
+            for (var i = 0; i < 5; i++) {
+                $("<span>").appendTo(element);
+            }
+            // Handle mouse events on the stars
+            $("span", element).each(function (index) {
+                $(this).hover(
+                    function () {
+                        $(this).prevAll().add(this).addClass("hoverChosen");
+                    },
+                    function () {
+                        $(this).prevAll().add(this).removeClass("hoverChosen");
+                    }).click(function () {
+                        var ratingObservable = valueAccessor(); // Get the associated observable
+                        ratingObservable(index + 1); // Write the new rating to it
+                    });
+            });
+        },
+        update: function (element, valueAccessor) {
+            // Give the first x stars the "chosen" class, where x <= rating
+            var ratingObservable = valueAccessor();
+            $("span", element).each(function (index) {
+                $(this).toggleClass("chosen", index < ratingObservable());
+            });
+        }
+    };
+
 })();
