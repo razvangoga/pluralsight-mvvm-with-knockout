1d768c15a6ec76b6c63e3e0ffe9826987e4f918f
razvan goga <razvan.goga@gmail.com>
2012-10-04 10:42:19 +0300

    Setup for Module 6 Demo 6

---
+++ KnockoutMVVM Module 6/KnockoutMVVM/scripts/knockout.changetracker.js
@@ -0,0 +1,40 @@
+﻿// Knockout.ChangeTracker
+//
+// John Papa http://johnpapa.net
+//                      http://twitter.com/@john_papa
+// Depends on scripts:
+//                         Knockout 
+//
+//  Notes:
+//              Special thanks to Steve Sanderson for his influence and help on this change tracker.
+//
+//  Usage:      
+//              To Setup Tracking, add this tracker property to your viewModel    
+//                                                     ===> viewModel.tracker = new ChangeTracker(viewModel);
+//
+//              Hook these into your view ...
+//                  Did It Change?          ===> viewModel.tracker().somethingHasChanged();
+//
+//              Hook this into your view model functions (ex: load, save) ...
+//                  Resync Changes        ===> viewModel.tracker().markCurrentStateAsClean;
+//
+//              Optionally, you can pass your own hashFunction for state tracking.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+(function (ko) {
+    "use strict";
+    ko.ChangeTracker = function (objectToTrack, hashFunction) {
+        hashFunction = hashFunction || ko.toJSON;
+        var lastCleanState = ko.observable(hashFunction(objectToTrack)),
+            result = {
+                somethingHasChanged: ko.computed(function () {
+                    return hashFunction(objectToTrack) !== lastCleanState();
+                }),
+                markCurrentStateAsClean: function () {
+                    lastCleanState(hashFunction(objectToTrack));
+                }
+            };
+        return function () {
+            return result;
+        };
+    };
+}(ko));
